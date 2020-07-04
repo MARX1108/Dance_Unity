f73f6c9a661d1807e7e334987d0499dc9ab928d8
Marx.W <boyuan@vt.edu>
2020-07-04 14:17:53 -0400

    init

---
+++ Assets/Scripts/NewTrainingButton.cs
@@ -0,0 +1,32 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+public class NewTrainingButton : MonoBehaviour
+{
+    public GameObject theText;
+    public GameObject thePanel;
+
+    public GameObject newTrainingPanel;
+
+
+
+
+    public void ClearText()
+    {
+        theText.GetComponent<InputField>().text = "";
+    }
+    public void CancelButton()
+    {
+        thePanel.SetActive(false);
+    }
+    public void OpenPanel()
+    {
+        thePanel.SetActive(true);
+    }
+    public void QuitButton()
+    {
+        Application.Quit();
+    }
+}
+
